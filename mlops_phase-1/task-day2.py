74ca438006ab6ed733b5feaa18c047eab16ed4f6
Your Name <usmanmub07@gmail.com>
2026-02-12 23:54:18 +0500

    model sampling with formating

--- mlops_phase-1/task-day2.py
+++ mlops_phase-1/task-day2.py
@@ -3,12 +3,12 @@ dataset_name = "T2 Dataset"
 accuracy = 0.91 
 accuracy_percent = accuracy * 100
 
-print(f" Model: {model_name} trained on {dataset_name} with achieved accuracy  {accuracy_percent:.2f}%")
+#print(f" Model: {model_name} trained on {dataset_name} with achieved accuracy  {accuracy_percent:.2f}%")
 
 total_sample = 1000
 correct_predictions = 873 
 accuracy = correct_predictions / total_sample
-#print(f" accuracy: {accuracy*100:.2f}%")
+print(f" accuracy: {accuracy*100:.2f}%")
 
 
 number = 42
